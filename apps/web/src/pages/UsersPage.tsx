e5022c195804bfa56b5d5463b28d58c540582419
Anderson <andersonfrfilho@gmail.com>
2026-06-22 20:42:09 -0300

    fix: remover Button import não utilizado em UsersPage

--- apps/web/src/pages/UsersPage.tsx
+++ apps/web/src/pages/UsersPage.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { Button } from '@/components/ui'
 import { api } from '@/lib/api'
 
 type User = {
